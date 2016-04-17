9214118642c374b2890aa5258461656d4b201b19
Regan Shaner <regdshaner@gmail.com>
2016-04-16 20:05:20 -0400

    Quick commit
    quick

--- app/src/main/java/regan/hackru/tagger/MainActivity.java
+++ app/src/main/java/regan/hackru/tagger/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements
 
 
         mapper = new DynamoDBMapper(ddbClient);
-
+ 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
