dcf34cf609894a98367b48364333c92460e5b661
Regan Shaner <regdshaner@gmail.com>
2016-04-16 23:11:34 -0400

    Revert "Revert "Moved tag generation to add tag instead of MainActivity""
    This reverts commit 5cc1b8eefbe882169ffd2cbb61e286e436f07759.

--- app/src/main/java/regan/hackru/tagger/AddTagActivity.java
+++ app/src/main/java/regan/hackru/tagger/AddTagActivity.java
@@ -1,30 +1,57 @@
 package regan.hackru.tagger;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class AddTagActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+public class AddTagActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener {
 
     Button takePhoto;
     Button textTool;
     Button drawTool;
+    Button submitButton;
 
     DrawView draw;
 
     boolean textToolActive = false;
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+
+    protected DynamoDBMapper mapper;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     /**
@@ -37,6 +64,40 @@ public class AddTagActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tag);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(AppIndex.API).build();
+        }
+
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-1:72789533-4d1e-4bcc-ba0b-afa67707fcd2", // Identity Pool ID
+                Regions.US_EAST_1 // Region
+        );
+
+        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+
+
+        mapper = new DynamoDBMapper(ddbClient);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationTag tag = buildTag();
+
+                SaveTagParams params = new SaveTagParams(mapper, tag);
+                new SaveTagTask().execute(params, null, null);
+            }
+        });
 
         textTool = (Button) findViewById(R.id.text_tool);
         textTool.setOnClickListener(new View.OnClickListener() {
@@ -84,16 +145,25 @@ public class AddTagActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private LocationTag buildTag() {
+        LocationTag tag = new LocationTag();
+        tag.setAuthor("Admin");
+        tag.setLocation(mLastLocation);
 
+        return tag;
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
-                "AddTag Page", // TODO: Define a title for the content shown.
+                "Main Page", // TODO: Define a title for the content shown.
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
@@ -101,18 +171,18 @@ public class AddTagActivity extends AppCompatActivity {
                 // TODO: Make sure this auto-generated app URL is correct.
                 Uri.parse("android-app://regan.hackru.tagger/http/host/path")
         );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
     }
 
     @Override
-    public void onStop() {
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
         super.onStop();
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
-                "AddTag Page", // TODO: Define a title for the content shown.
+                "Main Page", // TODO: Define a title for the content shown.
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
@@ -120,7 +190,70 @@ public class AddTagActivity extends AppCompatActivity {
                 // TODO: Make sure this auto-generated app URL is correct.
                 Uri.parse("android-app://regan.hackru.tagger/http/host/path")
         );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            Log.e("LOC", Double.toString(mLastLocation.getLatitude()));
+            Log.e("LOC", Double.toString(mLastLocation.getLongitude()));
+        }
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private static class SaveTagParams {
+        DynamoDBMapper mapper;
+        LocationTag t;
+
+        SaveTagParams(DynamoDBMapper mapper, LocationTag t) {
+            this.mapper = mapper;
+            this.t = t;
+        }
+
+        DynamoDBMapper getMapper() {
+            return mapper;
+        }
+
+        LocationTag getTag() {
+            return t;
+        }
+
+
+    }
+
+    private class SaveTagTask extends AsyncTask<SaveTagParams, Void, Void> {
+        @Override
+        protected Void doInBackground(SaveTagParams... params) {
+            DynamoDBMapper mapper = params[0].getMapper();
+            LocationTag t = params[0].getTag();
+
+            mapper.save(t);
+
+            return null;
+        }
     }
 }
